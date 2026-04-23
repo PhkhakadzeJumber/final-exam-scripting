cd0234f3980c689b02626d607a421b6d486241e4
juba phkhakadze <Phkhakadze.Jumber@kiu.edu.ge>
2026-04-23 15:25:10 +0400

    added ci/cd pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders 3 products on default category', () => {
+  render(
+    <MemoryRouter initialEntries={['/category/man']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const productCards = screen.getAllByRole('img');
+  expect(productCards.length).toBe(3);
 });
